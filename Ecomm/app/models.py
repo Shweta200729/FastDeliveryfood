814a12dddbf8a50f2e767838a517757a457df3c2
raj21121609 <raj.shukla21121609@gmail.com>
2025-10-19 11:02:40 +0530

    added and map and made changes in ui and also the implemented the booking table functionality

--- Ecomm/app/models.py
+++ Ecomm/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Booking(models.Model):
+    person = [
+        ('1','1'),
+        ('2','2'),
+        ('3','3'),
+        ('4','4'),
+        ('5','5')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+    phone_no = models.CharField(max_length=10, unique=True)
+    email = models.EmailField()
+    person_count = models.IntegerField(choices=person)
+    date = models.DateField()
+    
--- Ecomm/app/views.py
+++ Ecomm/app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Booking
 
 # Create your views here.
 def home (request):
@@ -7,13 +8,32 @@ def home (request):
 def landing (request):
     return render (request , "app/landing.html")   
 
-def menu (request):
-    return render (request, "app/menu.html")
-    
+def menu(request):
+    context = {
+        "lat": 18.9228,
+        "lon": 72.8317,
+        "api_key": "c22c22aa0aed46e5bf788d2004d335b3",
+    }
+    if request.method == 'POST':
+        return redirect('cart')
+    return render(request, "app/menu.html", context)
+
 def cart (request):
     return render (request, "app/cart.html")
 
-def BookingTable (request):
-    return render (request, "app/Bookingtable.html")
+def BookingTable(request):
+    if request.method == 'POST':
+        user = request.user
+        Booking.objects.create(
+            user=user,
+            name=request.POST.get('name'),
+            phone=request.POST.get('phone'),
+            email=request.POST.get('email'),
+            person_count=request.POST.get('person_count'),
+            date=request.POST.get('date'),
+        )
+        return redirect('menu')
+    return render(request, "app/Bookingtable.html")
+
     
     
