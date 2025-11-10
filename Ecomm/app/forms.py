43ccbd7368796dfe2fd3ab14c272f037cf792c7e
raj21121609 <raj.shukla21121609@gmail.com>
2025-11-10 22:08:40 +0530

    Refactor user creation form: streamline fields and validation logic

--- Ecomm/app/forms.py
+++ Ecomm/app/forms.py
@@ -1,24 +1,17 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django import forms
 
 class CreateUser(UserCreationForm):
     email = forms.EmailField(required=True)
-    model = User
-    fields = ['username', 'password1', 'email']
-    
-    def clean(self):
-        cleaned_data = super(UserCreationForm, self).clean()
-        password1 = cleaned_data.get("password1")
 
-        if not password1:
-            raise forms.ValidationError("Password is required.")
-        return cleaned_data
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
     def save(self, commit=True):
-        user = super(UserCreationForm, self).save(commit=False)
-        password = self.cleaned_data["password1"]
-        user.set_password(password)
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
         if commit:
             user.save()
         return user
